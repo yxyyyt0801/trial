abbe604f6e7d5bbc157ae296003054b3dab57f97
Rain <yxyyyt0801@gmail.com>
2025-05-21 13:10:53 +0800

    test: Add unsafe testcase

--- trial-jdk/src/test/java/com/sciatta/trial/jdk/unsafe/UnsafeTests.java
+++ trial-jdk/src/test/java/com/sciatta/trial/jdk/unsafe/UnsafeTests.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by yangxiaoyu on 2025/5/19<br>
@@ -19,53 +20,109 @@ public class UnsafeTests {
         // 类必须由 BootstrapClassLoader 加载，否则有安全隐患
         Unsafe.getUnsafe();
     }
-    
+
     @Test
     public void testGetUnsafe() {
         Unsafe unsafe = getUnsafe();    // 通过反射获得
         assertNotNull(unsafe);
     }
-    
+
     @Test
     public void testMemory() {
         Unsafe unsafe = getUnsafe();
-        
+
         int size = 4;
         long addr = unsafe.allocateMemory(size);    // 分配 4 字节长度的内存
         long addr3 = unsafe.reallocateMemory(addr, size * 2);   // 重新分配 8 字节长度的内存
         System.out.println("addr: " + addr);
         System.out.println("addr3: " + addr3);
-        
+
         try {
             // 起始字节地址addr的全部4个字节赋值 1
             // 00000001 00000001 00000001 00000001
             unsafe.setMemory(null, addr, size, (byte) 1);
-            
+
             for (int i = 0; i < 2; i++) {
                 // 00000001 00000001 00000001 00000001 00000001 00000001 00000001 00000001
                 unsafe.copyMemory(null, addr, null, addr3 + size * i, 4);
             }
-            
-            System.out.println(unsafe.getInt(addr));    // 16843009
-            System.out.println(unsafe.getLong(addr3));  // 72340172838076673
-            
+
+            assertEquals(16843009, unsafe.getInt(addr));
+            assertEquals(72340172838076673L, unsafe.getLong(addr3));
         } finally {
-            unsafe.freeMemory(addr);
-            unsafe.freeMemory(addr3);
+            //unsafe.freeMemory(addr);
+            //unsafe.freeMemory(addr3);
         }
     }
-    
+
     @Test
-    public void testSetField(){
+    public void testSetField() throws NoSuchFieldException {
         class Obj {
             private int value;
         }
-        
+
         Unsafe unsafe = getUnsafe();
-        
-        //unsafe.objectFieldOffset()
+
+        long valueOffset = unsafe.objectFieldOffset(Obj.class.getDeclaredField("value"));
+        Obj obj = new Obj();
+        unsafe.putInt(obj, valueOffset, 100);
+        assertEquals(100, obj.value);
     }
-    
+
+    @Test
+    public void testCAS() throws Exception {
+        class Obj {
+            private int value;
+        }
+
+        Unsafe unsafe = getUnsafe();
+        CountDownLatch latch = new CountDownLatch(2);
+        Obj obj = new Obj();
+        obj.value = 0;
+        int times = 1000000;
+        long valueOffset = unsafe.objectFieldOffset(Obj.class.getDeclaredField("value"));
+
+        Thread thread1 = new Thread(() -> {
+            int i = 0;
+            while (i < (times / 2)) {
+
+                try {
+                    plusOne(unsafe, obj, valueOffset);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+                i++;
+            }
+            latch.countDown();
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(() -> {
+            int i = 0;
+            while (i < (times / 2)) {
+                try {
+                    plusOne(unsafe, obj, valueOffset);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+                i++;
+            }
+            latch.countDown();
+        });
+        thread2.start();
+
+        latch.await();
+
+        assertEquals(times, obj.value);
+    }
+
+    private void plusOne(Unsafe unsafe, Object obj, long valueOffset) throws NoSuchFieldException {
+        int anInt = unsafe.getInt(obj, valueOffset);
+        while (!unsafe.compareAndSwapInt(obj, valueOffset, anInt, ++anInt)) {
+            anInt = unsafe.getInt(obj, valueOffset);
+        }
+    }
+
     private Unsafe getUnsafe() {
         try {
             Field field = Unsafe.class.getDeclaredField("theUnsafe");
